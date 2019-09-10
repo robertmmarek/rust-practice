4e26a47534a51e6bc7bc3aaf60403ff1a47c761a
robertmmarek <robertmmarek@gmail.com>
2019-09-10 15:46:01 +0200

    chapter 2, task 6 done

--- chapter_2/task_6/src/main.rs
+++ chapter_2/task_6/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("error reading");
+    let input_number: i32 = input.trim().parse::<i32>().expect("error parsing");
+
+    match input_number{
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("{}", input_number),
+        _ => println!("NaN")
+    }
 }
