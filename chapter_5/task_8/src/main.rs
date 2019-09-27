296e2b5998a1f8ff77148f3d1de7f27267ce5aee
robertmmarek <robertmmarek@gmail.com>
2019-09-27 13:54:38 +0200

    task_8 done

--- chapter_5/task_8/src/main.rs
+++ chapter_5/task_8/src/main.rs
@@ -1,3 +1,20 @@
+trait Mult{}
+
+impl Mult for usize{
+}
+
+impl Mult for isize{
+}
+
+fn multiply<T: Mult+std::ops::Mul>(a: T, b: T) -> <T as std::ops::Mul>::Output{
+    a*b
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a_u: usize = 2;
+    let b_u: usize = 3;
+    let a_i: isize = 2;
+    let b_i: isize = 3;
+    println!("{}", multiply(a_u, b_u));
+    println!("{}", multiply(a_i, b_i));
 }
