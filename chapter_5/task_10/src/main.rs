82153b30a1eb8af547a038ba806b3064792d7ca2
robertmmarek <robertmmarek@gmail.com>
2019-09-30 09:00:56 +0200

    task 10 finished

--- chapter_5/task_10/src/main.rs
+++ chapter_5/task_10/src/main.rs
@@ -1,3 +1,15 @@
+use std::fmt;
+
+trait Hello{
+    fn say(&self) where Self: std::fmt::Display{
+        println!("Hello {}!", self);
+    }
+}
+
+impl Hello for str{}
+impl Hello for String{}
+
 fn main() {
-    println!("Hello, world!");
+  "World".say();
+  String::from("World2").say();
 }
